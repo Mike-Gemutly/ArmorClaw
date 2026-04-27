764d6d8b7dec46ffae40bf9be2e9c29edc09091e
ArmorClaw Installer <mikegemut@gmail.com>
2026-04-26 18:05:14 -0600

    feat(sidecar-java): add Maven project skeleton with proto generation

---
+++ sidecar-java/src/main/java/com/armorclaw/sidecar/Main.java
@@ -0,0 +1,11 @@
+package com.armorclaw.sidecar;
+
+/**
+ * Temporary scaffolding entry point.
+ * Will be replaced by ServerMain.java in T2 (gRPC Server Core).
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ArmorClaw Java Sidecar starting...");
+    }
+}
